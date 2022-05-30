4577c4ef12fe6e2ce5e62080f66b9fe745bb8640
keenlylan <kelly.campen@gmail.com>
2022-05-31 11:01:02 +1200

    get users for view users activity

--- app/src/main/java/com/example/bit603_a3_kellycampen/ViewUsersActivity.java
+++ app/src/main/java/com/example/bit603_a3_kellycampen/ViewUsersActivity.java
@@ -1,14 +1,33 @@
 package com.example.bit603_a3_kellycampen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class ViewUsersActivity extends AppCompatActivity {
+    public static UserDatabase_v2 userDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_users);
+
+        userDatabase = Room.databaseBuilder(getApplicationContext(), UserDatabase_v2.class, "userdb").allowMainThreadQueries().build();
+
+        final TextView textViewOutput = findViewById(R.id.textViewViewUsers_output);
+
+        List<User> users = userDatabase.dao().getUsers();
+        String output = "Item\t\t\tQuantity\t\t\tType\n";
+
+        for (User user : users) {
+            output += user.getUsername() + "\t\t\t" + user.getPassword() + "\t\t\t" + user.getEmployeeNumber().toString() + "\n";
+        }
+
+        textViewOutput.setText(output);
     }
 }
